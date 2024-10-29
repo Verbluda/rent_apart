adbe774c553059c9ca2601d92bb2670187f8d8cd
Lucy <mila.hairzamanova@gmail.com>
2024-10-29 13:08:57 +0300

    [RAA-005] intermediate result on automation

--- architect_module/src/main/java/com/example/architect_module/service/impl/ArchitectServiceImpl.java
+++ architect_module/src/main/java/com/example/architect_module/service/impl/ArchitectServiceImpl.java
@@ -5,35 +5,56 @@ import com.example.architect_module.service.ArchitectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLOutput;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class ArchitectServiceImpl implements ArchitectService {
     @Override
     public void createMigrationFile(ArchitectRequestDto architectRequestDto) {
         String operation = architectRequestDto.getOperation();
+        String name = architectRequestDto.getTableName();
+        Map<String, String> values = architectRequestDto.getValues();
 
-        //создать файл с версией, операцией и именем таблицы в названии
+        System.out.println(operation + " " + name);
+
+        //создать файл в src/main/resources/db/migration с версией, операцией и именем таблицы в названии
+
         switch (operation) {
             case "create":
-                // вызвать функцию для записи скрипта по созданию
+                writeInFileCreateScript(name, values);
                 System.out.println("create");
                 break;
             case "update":
-                // вызвать функцию для записи скрипта по апдейту
+                writeInFileUpdateScript(name, values);
                 System.out.println("update");
                 break;
-            case "drop":
-                // вызвать функцию для записи скрипта по удалению
-                System.out.println("drop");
+            case "delete":
+                writeInFileDeleteScript(name);
+                System.out.println("delete");
                 break;
             default:
-                // выбросить исключение
                 System.out.println("неизвестная операция с базой данных");
+                throw new RuntimeException();
         }
     }
 
     private int calculateVersionOfScript() {
+
         //вычисление версии скрипта с помощью  JDBCTemplate, отдельная функция без параметров
         return 1;
+    }
+
+    private void writeInFileCreateScript(String tableName, Map<String, String> value) {
+
+    }
+
+    private void writeInFileUpdateScript(String tableName, Map<String, String> value) {
+
+    }
+
+    private void writeInFileDeleteScript(String tableName) {
+
     }
 }
