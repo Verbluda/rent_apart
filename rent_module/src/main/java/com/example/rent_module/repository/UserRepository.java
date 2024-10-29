e1b37f1582e78aba3e4385d1a68f501491c09f7e
Lucy <mila.hairzamanova@gmail.com>
2024-10-29 17:12:18 +0300

    [RAA-009] планировщик

--- rent_module/src/main/java/com/example/rent_module/repository/UserRepository.java
+++ rent_module/src/main/java/com/example/rent_module/repository/UserRepository.java
@@ -4,10 +4,13 @@ import com.example.rent_module.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     @Query(value = "select a from UserEntity a where a.token = :token")
     Optional<UserEntity> findUserByToken(String token);
+
+    List<UserEntity> findUserEntitiesByTokenIsNotNull();
 }
