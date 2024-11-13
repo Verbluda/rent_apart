bea5a29ca683fc922dd1e292c8f518bc04050008
Lucy <mila.hairzamanova@gmail.com>
2024-11-13 13:50:57 +0300

    [RAA-005] коряво работает update

--- architect_module/src/main/java/com/example/architect_module/service/impl/ArchitectServiceImpl.java
+++ architect_module/src/main/java/com/example/architect_module/service/impl/ArchitectServiceImpl.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -20,15 +23,20 @@ public class ArchitectServiceImpl implements ArchitectService {
     public static final String CREATE_SCRIPT_START = "CREATE TABLE IF NOT EXISTS %s (\n    id int8 PRIMARY KEY";
     public static final String COLUMNS = ",\n    %s %s";
     public static final String CREATE_SCRIPT_END = ");\n\nCREATE SEQUENCE %s_sequence START 1 INCREMENT 1;";
-    public static final String UPDATE_SCRIPT_START = "ALTER TABLE ";
+    public static final String UPDATE_SCRIPT_START = "ALTER TABLE IF EXISTS {";
+    public static final String UPDATE_SCRIPT_ADD = "\nADD %s IF NOT EXISTS %s";
+    public static final String UPDATE_SCRIPT_DELETE = "\nDROP %s IF EXISTS";
+    public static final String UPDATE_SCRIPT_UPDATE_TYPE = "\nALTER %s %s";
+    public static final String UPDATE_SCRIPT_END = "\n};";
     public static final String DELETE_SCRIPT = "DROP TABLE IF EXIST %s;\n DROP SEQUENCE IF EXIST %s_sequence";
     private final JdbcTemplate jdbcTemplate;
+
     @Override
     public void createMigrationFile(ArchitectRequestDto architectRequestDto) {
         String operation = architectRequestDto.getOperation();
         System.out.println(operation);
         String name = architectRequestDto.getTableName();
-        Map<String, String> columns = architectRequestDto.getValues();
+        Map<String, String> newColumns = architectRequestDto.getValues();
 
         int version = calculateVersionOfScript();
 
@@ -40,10 +48,10 @@ public class ArchitectServiceImpl implements ArchitectService {
             StringBuilder script = new StringBuilder();
             switch (operation) {
                 case "create":
-                    writeInFileCreateScript(script, name, columns);
+                    writeInFileCreateScript(script, name, newColumns);
                     break;
                 case "update":
-                    writeInFileUpdateScript(script, name, columns);
+                    writeInFileUpdateScript(script, name, newColumns);
                     break;
                 case "delete":
                     System.out.println(operation);
@@ -65,9 +73,9 @@ public class ArchitectServiceImpl implements ArchitectService {
         return jdbcTemplate.queryForObject(query, Integer.class) + 1;
     }
 
-    private void writeInFileCreateScript(StringBuilder script, String tableName, Map<String, String> columns) {
+    private void writeInFileCreateScript(StringBuilder script, String tableName, Map<String, String> newColumns) {
         script.append(String.format(CREATE_SCRIPT_START, tableName));
-        for (Map.Entry<String, String> column : columns.entrySet()) {
+        for (Map.Entry<String, String> column : newColumns.entrySet()) {
             script.append(String.format(COLUMNS, column.getKey(), column.getValue()));
         }
         script.append(String.format(CREATE_SCRIPT_END, tableName));
@@ -75,11 +83,61 @@ public class ArchitectServiceImpl implements ArchitectService {
 
     private void writeInFileUpdateScript(StringBuilder script, String tableName, Map<String, String> newColumns) {
         script.append(String.format(UPDATE_SCRIPT_START, tableName));
-        String query = "SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = \"%s\"";
-        Map<String, Object> oldColumns = jdbcTemplate.queryForMap(String.format(query, tableName));
-        for (Map.Entry<String, Object> oldColumn : oldColumns.entrySet()) {
-            System.out.println(oldColumn.getKey() + oldColumn.getValue());
+
+        List<String> columnsToDelete = new ArrayList<>();
+        Map<String, String> columnsToUpdateType = new HashMap<>();
+        Map<String, String> columnsToAdd = new HashMap<>();
+
+        String query = "SELECT COLUMN_NAME, UDT_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '%s'";
+        List<Map<String, Object>> oldColumns = jdbcTemplate.queryForList(String.format(query, "address"));
+        List<String> oldColumnsNameList = new ArrayList<>();
+
+        for (Map<String, Object> oldColumn : oldColumns) {
+            String oldColumnName = oldColumn.get("column_name").toString();
+            oldColumnsNameList.add(oldColumnName);
+            if (!newColumns.keySet().contains(oldColumnName)) {
+                columnsToDelete.add(oldColumnName);
+            } else {
+                String oldColumnType = oldColumn.get("udt_name").toString();
+                String newColumnType = newColumns.get(oldColumnName);
+                if (!newColumnType.equals(oldColumnType)) {
+                    columnsToUpdateType.put(oldColumnName, newColumnType);
+                }
+            }
         }
+        for (String newColumnName : newColumns.keySet()) {
+            if (!oldColumnsNameList.contains(newColumnName)) {
+                columnsToAdd.put(newColumnName, newColumns.get(newColumnName));
+            }
+        }
+
+        if (!columnsToDelete.isEmpty()) {
+            for (int i = 0; i < columnsToDelete.size(); i++) {
+                script.append(String.format(UPDATE_SCRIPT_DELETE, columnsToDelete.get(i)));
+                if (i != columnsToDelete.size() - 1) {
+                    script.append(" | ");
+                }
+            }
+        }
+        if (!columnsToAdd.isEmpty()) {
+            if (!columnsToDelete.isEmpty()) {
+                script.append(" | ");
+            }
+            for (Map.Entry<String, String> columnToAdd : columnsToAdd.entrySet()) {
+                script.append(String.format(UPDATE_SCRIPT_ADD, columnToAdd.getKey(), columnToAdd.getValue()));
+                script.append(" | ");
+            }
+        }
+        if (!columnsToUpdateType.isEmpty()) {
+            for (Map.Entry<String, String> columnToUpdateType : columnsToUpdateType.entrySet()) {
+                script.append(String.format(UPDATE_SCRIPT_UPDATE_TYPE, columnToUpdateType.getKey(), columnToUpdateType.getValue()));
+                script.append(" | ");
+            }
+        }
+        if (!columnsToAdd.isEmpty() || !columnsToUpdateType.isEmpty()) {
+            script.delete(script.length() - 3, script.length() - 1);
+        }
+        script.append(UPDATE_SCRIPT_END);
     }
 
     private void writeInFileDeleteScript(StringBuilder script, String tableName) {
